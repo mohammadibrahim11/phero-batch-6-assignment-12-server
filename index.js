a31515eab9b8f18c7f66ef0aaae9b57824344799
Mohammad Ibrahim <iibrahim01081998@gmail.com>
2022-11-25 03:43:09 +0600

    creat database

--- index.js
+++ index.js
@@ -1,34 +1,50 @@
 const express = require("express");
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
-const category = require('./CategoryData/CategoryData.json');
-
-// const categories = require('./coursedata/categories.json');
-
-
-
-
-
-app.get('/category/:id',(req,res)=>{
-    const id = parseInt(req.params.id); 
-    const selectedCategory = category.find(c => c._id === id);
-    res.send(selectedCategory);
-})
-
-
-
-
-app.get('/',(req,res)=>{
-    res.send('Yes working!');
-
+app.get("/", (req, res) => {
+  res.send("Yes working!");
 });
 
-app.listen(5000, () => console.log(`server is running on port${port}`));
+const uri = `mongodb+srv://user5:VqE7tcAoCJNibvL9@cluster0.gbplfqy.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const categoryCollection = client
+      .db("reCommerce")
+      .collection("productsCategory");
+
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const cursor = categoryCollection.find(query);
+      const categories = await cursor.toArray();
+      res.send(categories);
+    });
+
+    app.get("/categories/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const categoriesDetails = await categoryCollection.findOne(query);
+        res.send(categoriesDetails);
+      });
+  } finally {
+  }
+}
+run().catch((err) => console.log(err));
+
+
+app.listen(port, () => console.log(`server is running on port ${port}`));
 
 //export the express API
 
